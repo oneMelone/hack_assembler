3773dd41dfed23ddc6559e7b70a6adbcd1a95842
oneMelone <1040835678@qq.com>
2022-08-19 22:24:01 +0800

    complete

--- code/code.go
+++ code/code.go
@@ -4,89 +4,91 @@ import (
 	"strconv"
 	"hack_assembler/symboltable"
 	"hack_assembler/parser"
+
+	"fmt"
 )
 
 const (
 	INSTRUCTION_SIZE = 16
 )
 
-var (
+type Translator struct {
 	compMap map[string]string
 	destMap map[string]string
 	jumpMap map[string]string
 	symbolTable symboltable.SymbolTable
-)
-
-func InitTranslator(st symboltable.SymbolTable) {
-	compMap := make(map[string]string)
-	compMap["0"] = "0101010"
-	compMap["1"] = "0111111"
-	compMap["-1"] = "0111010"
-	compMap["D"] = "0001100"
-	compMap["A"] = "0110000"
-	compMap["M"] = "1110000"
-	compMap["!D"] = "0001101"
-	compMap["!A"] = "0110001"
-	compMap["!M"] = "1110001"
-	compMap["-D"] = "0001111"
-	compMap["-A"] = "0110011"
-	compMap["-M"] = "1110011"
-	compMap["D+1"] = "0011111"
-	compMap["A+1"] = "0110111"
-	compMap["M+1"] = "1110111"
-	compMap["D-1"] = "0001110"
-	compMap["A-1"] = "0110010"
-	compMap["M-1"] = "1110010"
-	compMap["D+A"] = "0000010"
-	compMap["D+M"] = "1000010"
-	compMap["D-A"] = "0010011"
-	compMap["D-M"] = "1010011"
-	compMap["A-D"] = "0000111"
-	compMap["M-D"] = "1000111"
-	compMap["D&A"] = "0000000"
-	compMap["D&M"] = "1000000"
-	compMap["D|A"] = "0010101"
-	compMap["D|M"] = "1010101"
-
-	destMap := make(map[string]string)
-	destMap[""] = "000"
-	destMap["M"] = "001"
-	destMap["D"] = "010"
-	destMap["DM"] = "011"
-	destMap["A"] = "100"
-	destMap["AM"] = "101"
-	destMap["AD"] = "110"
-	destMap["ADM"] = "111"
-
-	jumpMap := make(map[string]string)
-	jumpMap[""] = "000"
-	jumpMap["JGT"] = "001"
-	jumpMap["JEQ"] = "010"
-	jumpMap["JGE"] = "011"
-	jumpMap["JLT"] = "100"
-	jumpMap["JNE"] = "101"
-	jumpMap["JLE"] = "110"
-	jumpMap["JMP"] = "111"
-
-	symbolTable = st
 }
 
-func translate(statement parser.Statement) string {
+func (t *Translator)InitTranslator(st symboltable.SymbolTable) {
+	t.compMap = make(map[string]string)
+	t.compMap["0"] = "0101010"
+	t.compMap["1"] = "0111111"
+	t.compMap["-1"] = "0111010"
+	t.compMap["D"] = "0001100"
+	t.compMap["A"] = "0110000"
+	t.compMap["M"] = "1110000"
+	t.compMap["!D"] = "0001101"
+	t.compMap["!A"] = "0110001"
+	t.compMap["!M"] = "1110001"
+	t.compMap["-D"] = "0001111"
+	t.compMap["-A"] = "0110011"
+	t.compMap["-M"] = "1110011"
+	t.compMap["D+1"] = "0011111"
+	t.compMap["A+1"] = "0110111"
+	t.compMap["M+1"] = "1110111"
+	t.compMap["D-1"] = "0001110"
+	t.compMap["A-1"] = "0110010"
+	t.compMap["M-1"] = "1110010"
+	t.compMap["D+A"] = "0000010"
+	t.compMap["D+M"] = "1000010"
+	t.compMap["D-A"] = "0010011"
+	t.compMap["D-M"] = "1010011"
+	t.compMap["A-D"] = "0000111"
+	t.compMap["M-D"] = "1000111"
+	t.compMap["D&A"] = "0000000"
+	t.compMap["D&M"] = "1000000"
+	t.compMap["D|A"] = "0010101"
+	t.compMap["D|M"] = "1010101"
+
+	t.destMap = make(map[string]string)
+	t.destMap[""] = "000"
+	t.destMap["M"] = "001"
+	t.destMap["D"] = "010"
+	t.destMap["DM"] = "011"
+	t.destMap["A"] = "100"
+	t.destMap["AM"] = "101"
+	t.destMap["AD"] = "110"
+	t.destMap["ADM"] = "111"
+
+	t.jumpMap = make(map[string]string)
+	t.jumpMap[""] = "000"
+	t.jumpMap["JGT"] = "001"
+	t.jumpMap["JEQ"] = "010"
+	t.jumpMap["JGE"] = "011"
+	t.jumpMap["JLT"] = "100"
+	t.jumpMap["JNE"] = "101"
+	t.jumpMap["JLE"] = "110"
+	t.jumpMap["JMP"] = "111"
+
+	t.symbolTable = st
+}
+
+func (t *Translator)Translate(statement parser.Statement) string {
 	if statement.InstructionType == parser.AInstruction {
-		return translateA(statement)
+		return t.translateA(statement)
 	} else {
-		return translateC(statement)
+		return t.translateC(statement)
 	}
 }
 
-func translateA(statement parser.Statement) string {
+func (t *Translator)translateA(statement parser.Statement) string {
 	var address int
 	var ok bool
 	var err error
-	if address, ok = symbolTable.GetSymbolValue(statement.Address); !ok {
+	if address, ok = t.symbolTable.GetSymbolValue(statement.Address); !ok {
 		address, err = strconv.Atoi(statement.Address)
 		if err != nil {
-			address = symbolTable.GetVarSymbolValue(statement.Address)
+			address = t.symbolTable.GetVarSymbolValue(statement.Address)
 		}
 	}
 
@@ -99,8 +101,23 @@ func translateA(statement parser.Statement) string {
 	return binaryRep
 }
 
-func translateC(statement parser.Statement) string {
+func (t *Translator)translateC(statement parser.Statement) string {
+	// ----test----
+	fmt.Println("before translate, map is")
+	fmt.Println("compMap:", t.compMap)
+	fmt.Println("destMap:", t.destMap)
+	fmt.Println("jumpMap:", t.jumpMap)
+
 	result := "111"
-	result += compMap[statement.Comp] + destMap[statement.Dest] + jumpMap[statement.Jump]
+
+	// ----test-----
+	fmt.Println("in translator")
+	fmt.Println("statement.Comp is", statement.Comp)
+	fmt.Println("comp is", t.compMap[statement.Comp])
+	fmt.Println("statement.Dest is", statement.Dest)
+	fmt.Println("dest is", t.compMap[statement.Dest])
+	fmt.Println("statement.Jump is", statement.Jump)
+
+	result += t.compMap[statement.Comp] + t.destMap[statement.Dest] + t.jumpMap[statement.Jump]
 	return result
 }
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"hack_assembler/symboltable"
+	"hack_assembler/code"
+	"hack_assembler/parser"
 	"os"
 	"bufio"
+
 	"fmt"
 )
 
@@ -16,14 +19,68 @@ func main() {
 	}
 	defer input.Close()
 
-	// init symboltable
+	// init symboltable and translator
 	st := symboltable.SymbolTable{}
 	st.InitSymbolTable()
+	t := code.Translator{}
+	t.InitTranslator(st)
 
 	// read file to string slice, and add label symbol
 	scanner := bufio.NewScanner(input)
+	pc := 0
+	inputRows := make([]string, 0)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line := scanner.Text()
+		// ignore empty line
+		if len(line) == 0 {
+			continue
+		}
+
+		targLine := ""
+		for _, c := range line {
+			if c != ' ' {
+				targLine += string(c)
+			}
+		}
+
+		// check if this is a label
+		if targLine[0] == '(' {
+			st.SetLabelSymbolValue(targLine[1:len(targLine) - 1], pc)
+			continue
+		}
+
+		inputRows = append(inputRows, targLine)
+		pc += 1
 	}
-	// parse each line, write to targetfile.
+
+	// parse each line, translate and write to targetfile.
+	statements := parser.Parse(inputRows)
+
+	// ----test----
+	fmt.Println("statements:")
+	for _, statement := range statements {
+		fmt.Println(statement)
+	}
+
+	mcodes := make([]string, 0)
+	for _, statement := range statements {
+		mcodes = append(mcodes, t.Translate(statement))
+	}
+
+	outputFile, err := os.OpenFile("out.hack", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		panic(nil)
+	}
+
+	defer outputFile.Close()
+
+	writer := bufio.NewWriter(outputFile)
+
+	for _, mcode := range mcodes {
+		fmt.Println("mcode is", mcode)
+		_, _ = writer.WriteString(mcode + "\n")
+	}
+
+	writer.Flush()
 }
